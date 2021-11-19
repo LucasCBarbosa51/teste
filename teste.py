805de690499c08a8e28d879972c6145290464bae
LucasCBarbosa51 <86112428+LucasCBarbosa51@users.noreply.github.com>
2021-11-19 13:31:52 -0300

    Atualizando solicitações de correções.

--- teste.py
+++ teste.py
@@ -3,11 +3,6 @@ Teste para um novo repositório
 """
 print('TESTE para simular uma nova branch')
 print('OUTRO TESTE')
-
-
-print('TESTE CONFLITO')
-
-print('TESTE CONFLITO ESCRITO NA BRANCH A')
-
+print('TESTE CONFLITO'.lower())
+print('TESTE CONFLITO ESCRITO NA BRANCH A'.lower())
 print('Mais um novo print')
-
