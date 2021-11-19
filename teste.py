dc579d6473be850c4b6377c92074d183bbcb4a9e
Lucas Barbosa <barbosa.lucas390@yahoo.com>
2021-11-19 10:57:25 -0300

    Adicionado commit na branch A

--- teste.py
+++ teste.py
@@ -4,6 +4,10 @@ Teste para um novo repositório
 print('TESTE para simular uma nova branch')
 print('OUTRO TESTE')
 
+
 print('TESTE CONFLITO')
 
+print('TESTE CONFLITO ESCRITO NA BRANCH A')
+
 print('Mais um novo print')
+
